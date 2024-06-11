787b50042857640e7a382aee1b48b74d35de55bb
abdumomin-ai <155010267+abdumomin-ai@users.noreply.github.com>
2024-06-11 19:21:21 +0500

    Bu yerga nima deb yozishni bilmayman
    OOP uchun ikkinchi mashg'ulotni kiritdim. Qolganini qila olmadim. Afsus.

---
+++ Car2.java
@@ -0,0 +1,10 @@
+public class Car2 {
+
+    public static void main(String[] args) {
+        Product4 product4 = new Product4("Blue","BMW", 1100000000, "Mechanic");
+        System.out.println(Product4.color);
+        System.out.println(Product4.model);
+        System.out.println(Product4.price);
+        System.out.println(Product4.type);
+    }
+}
---
+++ Product4.java
@@ -0,0 +1,13 @@
+public class Product4 {
+     static String color;
+     static String model;
+     static int price;
+     static String type;
+
+     Product4(String color, String model, int price, String type){
+         this.color = color;
+         this.model = model;
+         this.price = price;
+         this.type = type;
+     }
+}
